6fb1ae3df524008318c7c0faf49a03ba85d8b222
Twince <tjsdn811@gmail.com>
2025-12-17 20:36:08 +0900

    schema: 시리즈 기능을 위한 스키마 추가

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,6 @@
 import { defineCollection, z } from "astro:content";
 
-const SeoSchema = z.object({
+export const SeoSchema = z.object({
   title: z.string(),
   description: z.string(),
 });
@@ -10,13 +10,14 @@ const ThumbnailSchema = z.object({
   alt: z.string().optional()
 })
 
-const PostSchema = z.object({
+export const PostSchema = z.object({
   title: z.string(),
   description: z.string(),
   date: z.coerce.date(),
   status: z.enum(['draft', 'published']),
   tags: z.array(z.string()).optional().default([]),
   topic: z.string().optional(),
+  series: z.array(z.string()).optional().default([]),
   readingTime: z.number().optional(),
   author: z.string(),
   coAuthors: z.array(z.string()).optional().default([]),
