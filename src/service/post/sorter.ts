d3e4c8ad4a159f0315a05090a29649e0ba1253e7
Twince <tjsdn811@gmail.com>
2025-12-28 00:57:36 +0900

    feat: post간 publishedDate가 같을 시 제목순 정렬 기능 추가

--- src/service/post/sorter.ts
+++ src/service/post/sorter.ts
@@ -8,7 +8,9 @@ export const sortPosts = (posts: ResolvedPost[]): ResolvedPost[] => {
 // 날짜 내림차순
 const sortBylastest = (posts:ResolvedPost[]) => {
   return posts.sort((a, b) => {
-    return b.date.getTime() - a.date.getTime();
+    const timeDiff = b.date.getTime() - a.date.getTime();
+    if(timeDiff == 0) return timeDiff;
+    return a.title.localeCompare(b.title); // 게시 시간이 같다면 제목순으로 정렬
   })
 }
 
