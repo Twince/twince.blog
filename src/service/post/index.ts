e479b5a0c3e84a3f43a4b42d04004fb78c8e0ec3
Twince <tjsdn811@gmail.com>
2025-12-28 19:51:00 +0900

    feat: 카테고리를 포함하는 post 필터링 기능 구현

--- src/service/post/index.ts
+++ src/service/post/index.ts
@@ -6,6 +6,7 @@ import { sortPosts } from "./sorter";
 // type
 import type { CollectionEntry } from "astro:content";
 import type { ResolvedPost } from "./types/Resolver";
+import type { PostSummary } from "./types/Index";
 import type { NeighborPosts } from "./types/Index";
 
 
@@ -13,7 +14,7 @@ type rawPost = CollectionEntry<'posts'>
 
 export const PostService = {
   // status가 'publihed'인 Post만 fetch(프로덕트 빌드된 기준)
-  async getPublsihedPosts(): Promise<ResolvedPost[]> {
+  async getPublishedPosts(): Promise<ResolvedPost[]> {
     const rawPosts = await getCollection('posts', ({data}): rawPost => {
       return import.meta.env.PROD ? data.status === "published" : true;
     });
@@ -23,7 +24,7 @@ export const PostService = {
 
   // 특정 Post의 slug를 기준으로 이전 글과 다음 글 fetch
   async getPostWithNeighbors(slug: string): Promise<NeighborPosts> {
-    const allPosts = await this.getPublsihedPosts();
+    const allPosts = await this.getPublishedPosts();
     const currentIndex = allPosts.findIndex((post) => post.slug === slug);
 
     if(currentIndex === - 1) return null;
@@ -31,6 +32,7 @@ export const PostService = {
     const isFirstPost = 0 === currentIndex;
     const isLastPost = allPosts.length-1 === currentIndex;
 
+    //TODO: 포스트 존재 여부로 함수 리팩토링 하기(ex. !post ? return NeighborPosts : null)
     return {
       next: {
         slug: isFirstPost ? null : allPosts[currentIndex-1].slug,
@@ -43,5 +45,26 @@ export const PostService = {
         description: isLastPost ? null : allPosts[currentIndex+1].description
       }
     }
+  },
+
+  async getPostWithCategories(categories: string | string[]): Promise<PostSummary[]> {
+    const allPosts = await this.getPublishedPosts();
+    // 입력받은 카테고리를 uppercase된 string[] 형태로 정규화
+    const targetCategories = Array.isArray(categories) ? categories.map((c) => c.toUpperCase()) : [categories.toUpperCase()];
+
+    const categorySet = new Set(targetCategories);
+    const filteredPosts = allPosts.filter((post) => {
+      const postCategories = post.categories;
+      return postCategories.some((c) => categorySet.has(c))
+    })
+
+    return filteredPosts.map((post) => {
+      return {
+        slug: post.slug,
+        title: post.title,
+        description: post.description,
+        thumbnail: post.thumbnail,
+      }
+    })
   }
 }
--- src/service/post/types/Index.ts
+++ src/service/post/types/Index.ts
@@ -1,12 +1,14 @@
-export type NeighborPosts = {
-  next: {
-    slug: string | null,
-    title: string | null
-    description: string | null,
-  },
-  previous: {
+export type PostSummary = {
     slug: string | null;
     title: string | null;
     description: string | null;
-  }
+    thumbanil?: {
+      src: string;
+      alt: string;
+    }
+}
+
+export type NeighborPosts = {
+  next: PostSummary;
+  previous: PostSummary;
 } | null;
