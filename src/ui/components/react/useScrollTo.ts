42537e31c790c2319640a381a5f95a8399218701
Twince <tjsdn811@gmail.com>
2026-02-01 00:56:08 +0900

    chore: scrollTo의 offset 매개변수 값을 더 직관적으로 수정

--- src/ui/components/react/useScrollTo.ts
+++ src/ui/components/react/useScrollTo.ts
@@ -1,5 +1,5 @@
 export const useScrollTo = (containerId: string) => {
-  const scrollTo = (targetId: string, offset: number = -30 ) => {
+  const scrollTo = (targetId: string, offset: number = 30 ) => {
     const container = document.getElementById(containerId);
     const element = document.getElementById(targetId);
 
@@ -8,7 +8,7 @@ export const useScrollTo = (containerId: string) => {
     const containerTop = container.getBoundingClientRect().top;
     const elementTop = element.getBoundingClientRect().top;
 
-    const targetScrollTop = container.scrollTop + (elementTop - containerTop) + offset;
+    const targetScrollTop = container.scrollTop + (elementTop - containerTop) - offset;
     // 현재 scroll값 + ((viewport기준) 선택된 elementTop - containerTop으로 scroll container높이와 heading간 gap 계산) + offset
     container.scrollTo({
       top: targetScrollTop,
