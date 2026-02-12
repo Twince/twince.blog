3ce3aba490db2e4b9983aa6ce556148216d9f29c
Twince <tjsdn811@gmail.com>
2026-02-13 02:55:47 +0900

    feat: undisplayHeading 구현 및 세부 인터렉션 수정

--- src/ui/components/react/Toc.tsx
+++ src/ui/components/react/Toc.tsx
@@ -17,7 +17,8 @@ export const Toc = ({idList, flattenToc}: TocProps) => {
   const [sectionEndIndex, setSectionEndIndex] = useState<number | null>(0);
   const rootContainerId = 'article-wrapper'
   const rootDepth = 2;
-  
+  const hidingDepth = 6;
+
   const { scrollTo } = useScrollTo(rootContainerId);
   
   const scrollIntoView = (itemId: string) => {
@@ -40,15 +41,11 @@ export const Toc = ({idList, flattenToc}: TocProps) => {
       setSectionRootIndex(findSectionRootIndex(flattenToc, activeIdIndex, rootDepth));
       setSectionEndIndex(findSectionEndIndex(flattenToc, activeIdIndex, rootDepth));
 
-     
     };
     root.addEventListener("scroll", onChange);
-    console.log(flattenToc)
     
     return () => root.removeEventListener("scroll", onChange);
   }, [])
-
-  useEffect(() => console.log(currentActiveIndex) ,[currentActiveIndex])
 
   let rootheadingCount = 0;
   return(
@@ -56,21 +53,24 @@ export const Toc = ({idList, flattenToc}: TocProps) => {
     <h2 className={"font-black -traslate-x-4 underline-offset-2"}>TABLE OF CONTENTS</h2>
     <ul>
       {flattenToc.map((item, index) => {
+        console.log(sectionRootIndex)
         const activeBorderCondition = index >= sectionRootIndex && index <= sectionEndIndex;
         const activeTextCondition = index <= currentActiveIndex && index >= sectionRootIndex;
 
-        if(item.depth === rootDepth) rootheadingCount++;
+        const primaryHeading = item.depth === rootDepth || item.depth === hidingDepth;
+
+        if(primaryHeading) rootheadingCount++;
         return (
           <li key={item.id} className={"flex"} onClick={() => scrollIntoView(item.id)} >
-            <div key={item.id} toc-depth={item.depth} 
+            <div key={item.id} toc-depth={item.depth}
             className={clsx(
-              "toc-item flex relative hover:border-text-quaternary transition-[box-shadow] duration-300 ease-in-out",
+              "toc-item flex relative hover:border-te xt-quaternary transition-[box-shadow] duration-300 ease-in-out",
               activeBorderCondition ? "border-l-2 border-text-active! hover:shadow-[inset_1px_0_0_0_var(--color-text-active)]" : "border-l-1x border-bg-primary hover:shadow-[inset_1px_0_0_0_var(--color-text-quaternary)]",
               activeTextCondition ? "text-text-active!" : "text-text-primary",
-              item.depth === 2 ? "mt-7 h-7 pb-0.5" : "mt-0",
+              currentActiveIndex === index ? "shadow-[inset_0.5px_0_0_0_var(--color-text-active)]" : "",
               index === 0 && "mt-4!"
               )}>
-                {item.depth === rootDepth && (<p className={"root-index"}>{String(rootheadingCount).padStart(2, '0')}</p>)}
+                {primaryHeading && (<p className={"root-index"}>{String(rootheadingCount).padStart(2, '0')}</p>)}
                 <div className={"pl-10"}><p className={"hover:translate-x-0.5 rounded-[1px] duration-300 ease-out"}>{`${item.text}`}</p></div>
             </div>
           </li>
