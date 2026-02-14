cd9137e8297bfec52b09dacf721983d5ba083524
Twince <tjsdn811@gmail.com>
2026-02-14 17:32:40 +0900

    fix: 글자가 길어지면 UI가 깨지던 이슈 해결

--- src/ui/components/react/Toc.tsx
+++ src/ui/components/react/Toc.tsx
@@ -61,17 +61,17 @@ export const Toc = ({idList, flattenToc}: TocProps) => {
 
         if(primaryHeading) rootheadingCount++;
         return (
-          <li key={item.id} className={"flex"} onClick={() => scrollIntoView(item.id)} >
+          <li key={item.id} className={"flex max-w-[30ch]"} onClick={() => scrollIntoView(item.id)} >
             <div key={item.id} toc-depth={item.depth}
             className={clsx(
-              "toc-item flex relative hover:border-te xt-quaternary transition-[box-shadow] duration-300 ease-in-out",
+              "toc-item flex h-fit relative hover:border-text-quaternary transition-[box-shadow] duration-300 ease-in-out",
               activeBorderCondition ? "border-l-2 border-text-active! hover:shadow-[inset_1px_0_0_0_var(--color-text-active)]" : "border-l-1x border-bg-primary hover:shadow-[inset_1px_0_0_0_var(--color-text-quaternary)]",
               activeTextCondition ? "text-text-active!" : "text-text-primary",
               currentActiveIndex === index ? "shadow-[inset_0.5px_0_0_0_var(--color-text-active)]" : "",
               index === 0 && "mt-4!"
               )}>
                 {primaryHeading && (<p className={"root-index"}>{String(rootheadingCount).padStart(2, '0')}</p>)}
-                <div className={"pl-10"}><p className={"hover:translate-x-0.5 rounded-[1px] duration-300 ease-out"}>{`${item.text}`}</p></div>
+                <div className={"pl-10 h-fit"}><p className={"hover:translate-x-0.5 rounded-[1px] duration-300 ease-out"}>{`${item.text}`}</p></div>
             </div>
           </li>
         )})}
